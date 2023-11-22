1652ad8917992730a5c717a9e53a831b11901313
Profiremu23 <profirdani23@gmail.com>
2023-11-22 19:48:25 +0100

    Added highly WIP code on getting the result

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,4 +1,8 @@
 def stock_picker(array)
+  result = Array.new
+  p buying_day = array[1]
+  p selling_day = array[4]
+  p result.push(buying_day, selling_day)
 end
 
 stock_picker([7,5,6,15,17,11,5,4,8,6])
