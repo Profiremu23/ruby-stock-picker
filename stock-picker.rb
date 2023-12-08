0e5db6f12dfdde711dae84b7a921528cf6eff726
Profiremu23 <profirdani23@gmail.com>
2023-12-08 19:48:00 +0100

    Part one of fixing buy and sell stock iteration

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,21 +1,26 @@
 def stock_picker(stock_array)
   stock_array.each_with_index do |price, index|
-    price = stock_array
-    index = 0
-    buying_stock_checker = price[index]
-    selling_stock_checker = price[index += 1]
-    buying_stock = buying_stock_checker
-    selling_stock = selling_stock_checker
-    max_profit = selling_stock - buying_stock
-    puts buying_day = buying_stock_checker[index]
-    puts selling_day = selling_stock_checker[index]
+    buying_stock = price
+    selling_stock = stock_array[index + 1]
 
+    if buying_stock.nil?
+      buying_stock = price[index]
+    end
+    if selling_stock.nil?
+      selling_stock = stock_array[index]
+    end
+
+    buying_day = buying_stock[index]
+    selling_day = selling_stock[index]
+    max_profit = selling_stock - buying_stock
+    
     #if selling_stock < buying_stock
-    #  selling_stock_checker[index + 1]
+    #  selling_stock = price
     #end
 
-    #if buying_stock < buying_stock_checker
-    #  buying_stock = buying_stock_checker
+    #if buying_stock < selling_stock
+    #  buying_stock_checker = stock_array[index += 1]
+    #  selling_stock_checker = stock_array[index += 1]
     #end
 
     #if selling_stock < selling_stock_checker
@@ -25,8 +30,10 @@ def stock_picker(stock_array)
     #if (selling_stock - buying_stock) > max_profit
     #  max_profit = selling_stock - buying_stock
     #end
-    #puts max_profit
+
+    result = Array.new
+    result.push(buying_day, selling_day)
   end
 end
 
-stock_picker([7, 5, 6, 15, 17, 11, 5, 4, 8, 6])
+stock_picker([7, 5, 6, 15, 17, 11, 5, 4, 8, 9])
